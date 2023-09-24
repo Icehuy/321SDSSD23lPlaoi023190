91b33ea4b60f3b6dcffa3a2efca8d4e2686ee41c
$tIX <82320835+Icehuy@users.noreply.github.com>
2023-09-24 17:42:30 +0300

    Update database.lua

--- database.lua
+++ database.lua
@@ -1,4 +1,5 @@
 local hwidlist = {
     ["russianware_randomKey_FISDJIJiji4230JKLDJFNSJKLVHJXCVBHJKADJFJKBHJAHE234198487123467846_stix"] = "72a9954f150cc7c11a02fd18624c1dbf";
+    ["russianware_111111r1andomKey_FsukaBLyattyebalrotSDJIJiEW4350JKLDJFNSzzzFJKBHJAHEV_SW34198487123467846_hashion"] = "bb58a925103cd92a10cdbb9464aed967";
 }
 return hwidlist
