562ca5bae0793f55c2492a951760cfe87ea7c4fb
$tIX <82320835+Icehuy@users.noreply.github.com>
2023-09-25 08:41:24 +0300

    Update database.lua

--- database.lua
+++ database.lua
@@ -3,6 +3,7 @@ local hwidlist = {
     ["russianware_646342r1andomKey_FsukaBLyattyeb5645ssdsadaotSDJIJiEJKLDSzzFKBJAHEV_SW341948234670000006_dexsai"] = "ed14ae9af74c4de848cfd1515e89ccbf";
     ["russianware_randomKey_FISDJIJiji4230JKLDJFNSJKLVHJXCVBHJKADJFJKBHJAHE234198487123467846_stix"] = "72a9954f150cc7c11a02fd18624c1dbf";
     ["nihuyadalbaeb_russianware_randomKey_FISDJIJiji423_eMae_He6aHbTE"] = "D1969158D9D4C560F2986C74235485406786";
+    ["russianware_randomKey_U_1_IASDUIOHUIOH3_12030102_9876634_ls428014nvcxnmarabiclsad_sukablyast_vini762"] = "7fc5284605a4a6bef2ea2ddd31313584";
 
 }
 return hwidlist
