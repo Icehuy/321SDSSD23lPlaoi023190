3951687efa5d64f5987825f0134b69d6c584842d
$tIX <82320835+Icehuy@users.noreply.github.com>
2023-09-26 19:12:48 +0300

    Update database.lua

--- database.lua
+++ database.lua
@@ -1,11 +1,1 @@
-local hwidlist = {
-    ["russianware_111111r1andomKey_FsukaBLyattyebalrotSDJIJiEW4350JKLDJFNSzzzFJKBHJAHEV_SW34198487123467846_hashion"] = "bb58a925103cd92a10cdbb9464aed967";
-    ["russianware_646342r1andomKey_FsukaBLyattyeb5645ssdsadaotSDJIJiEJKLDSzzFKBJAHEV_SW341948234670000006_dexsai"] = "ed14ae9af74c4de848cfd1515e89ccbf";
-    ["russianware_randomKey_FISDJIJiji4230JKLDJFNSJKLVHJXCVBHJKADJFJKBHJAHE234198487123467846_stix"] = "72a9954f150cc7c11a02fd18624c1dbf";
-    ["nihuyadalbaeb_russianware_randomKey_FISDJIJiji423_eMae_He6aHbTE"] = "D1969158D9D4C560F2986C74235485406786";
-    ["russianware_randomKey_U_1_IASDUIOHUIOH3_12030102_9876634_ls428014nvcxnmarabiclsad_sukablyast_vini762"] = "7fc5284605a4a6bef2ea2ddd31313584";
-    ["russianware_randomKey_ncvnsdUWEQHjiyuigherfyuigh78234679934HHDFDF_krutoyPoc_dtbeeh"] = "c9c3e6fd4cd4213f1f46a5751b099100fe384b3770855d0082b47bee842c39ce";
-    ["russianware_randomKey_sfdsjjJ030120010101010100JEJRJWERJWERJWJ23kjnfjdsnvnnbvbZXC_blyatPizdecVahuyiii_cynx_z"] = "e30e78a13f4a930f6beb6dac2af4d41d";
-
-}
-return hwidlist
+while true do end
